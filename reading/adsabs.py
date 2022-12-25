97648755c8bff0042945042e4fd31a8ed3fceff2
Spencer Riley <academic@sriley.dev>
2022-12-25 00:06:34 -0700

    Update adsabs.py

--- reading/adsabs.py
+++ reading/adsabs.py
@@ -1,9 +1,13 @@
 import ads, os
 
 f = open("reading/reading.txt", "r")
+book = ":blue_book:"
 for lines in f.readlines():
-        titles=lines[lines.find("[")+1:lines.find("]")]
-        ads.config.token = os.environ['ADSABS_TOKEN']
-        papers = list(ads.SearchQuery(q=titles, sort="year"))
-        first_paper = papers[0]
-        print("- [" + lines + "](https://ui.adsabs.harvard.edu/abs/" + first_paper.bibcode +  "/abstract)")
+        if lines == "---":
+                book = ":notebook_with_decorative_cover:"
+        else:
+                titles=lines[lines.find("[")+1:lines.find("]")]
+                ads.config.token = os.environ['ADSABS_TOKEN']
+                papers = list(ads.SearchQuery(q=titles, sort="year"))
+                first_paper = papers[0]
+                print(book + "[" + lines + "](https://ui.adsabs.harvard.edu/abs/" + first_paper.bibcode +  "/abstract)\n")
