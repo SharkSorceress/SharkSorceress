995c163de515017122cdb15736d02c838b03ca50
Spencer Riley <academic@sriley.dev>
2022-12-24 22:57:26 -0700

    Create adsabs.py

---
+++ reading/adsabs.py
@@ -0,0 +1,11 @@
+import ads
+
+f = open("./reading.txt", "r")
+for lines in f.readlines():
+        titles=lines[lines.find("[")+1:lines.find("]")]
+        ads.config.token = '{ADSABS_TOKEN}'
+        papers = list(ads.SearchQuery(q=titles, sort="year"))
+        first_paper = papers[0]
+        print("https://ui.adsabs.harvard.edu/abs/" + first_paper.bibcode +  "/abstract")
+
+
