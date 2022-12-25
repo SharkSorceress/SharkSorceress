1b93801d87c1852606c5a9c5ecc26eda44f79136
Spencer Riley <academic@sriley.dev>
2022-12-24 23:20:41 -0700

    Update adsabs.py

--- reading/adsabs.py
+++ reading/adsabs.py
@@ -1,9 +1,9 @@
 import ads
 
-f = open("./reading.txt", "r")
+f = open("reading/reading.txt", "r")
 for lines in f.readlines():
         titles=lines[lines.find("[")+1:lines.find("]")]
-        ads.config.token = '{{secrets.ADSABS_TOKEN}}'
+        ads.config.token = '${{ secrets.ADSABS_TOKEN }}'
         papers = list(ads.SearchQuery(q=titles, sort="year"))
         first_paper = papers[0]
         print("https://ui.adsabs.harvard.edu/abs/" + first_paper.bibcode +  "/abstract")
