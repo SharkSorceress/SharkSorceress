69455dfd56c13de41ee6323e1deaa5cb5618270b
Spencer Riley <academic@sriley.dev>
2022-12-26 18:57:32 -0700

    Update sdogif.py

--- py/sdogif.py
+++ py/sdogif.py
@@ -8,6 +8,6 @@ channel = [193, 304, 171, 211, 131, 335, 1600]
 url = "https://sdo.gsfc.nasa.gov/assets/img/latest/mpeg/latest_512_" + str(channel[datetime.now().weekday()]).zfill(4) + ".mp4"
 print(url)
 
-urllib.request.urlretrieve(url, "sdo_vid.gif")
-videoClip = VideoFileClip("sdo_vid.gif")
+urllib.request.urlretrieve(url, "sdo_vid.mp4")
+videoClip = VideoFileClip("sdo_vid.mp4")
 videoClip.speedx(10).write_gif("sdo_vid.gif", fps=10)
