78a3ccf9dcaf1e5ecc2fc9c5bbd2dc01191002f2
Spencer Riley <academic@sriley.dev>
2024-12-16 09:23:59 -0700

    Update sdogif.py

--- py/sdogif.py
+++ py/sdogif.py
@@ -1,6 +1,6 @@
 #from moviepy.editor import VideoFileClip
 from moviepy.video.io.VideoFileClip import VideoFileClip
-from moviepy.video.io.ffmpeg_writer import ffmpeg_write_video
+from moviepy.video.io.gif_writers import write_gif_with_imageio
 import moviepy.video.fx as fx #import MutiplySpeed, FadeOut
 import urllib.request
 import os
@@ -15,6 +15,7 @@ urllib.request.urlretrieve(url, "sdo_vid.mp4")
 videoClip = VideoFileClip("sdo_vid.mp4", target_resolution=(256, 256))
 videoClip = fx.MultiplySpeed(2).apply(videoClip)
 videoClip = fx.FadeOut(0.5).apply(videoClip)
-ffmpeg_write_video(videoClip, "sdo_vid.gif", 10)
+#ffmpeg_write_video(videoClip, "sdo_vid.gif", 10)
+write_gif_with_imageio(videoClip, "sdo_vid.gif", fps=10)
 # videoClip = VideoFileClip("sdo_vid.mp4").resize(height=256)
 #videoClip.speedx(2).fadeout(0.5).write_gif("sdo_vid.gif", fps=10)
